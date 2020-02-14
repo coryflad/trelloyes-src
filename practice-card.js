449c9c43976e6a7de0fc2f837de4435f6ef7e8f3
Cory Fladhammer <cjfladham@hotmail.com>
2020-02-13 21:33:52 -0600

    added function to the buttons

--- practice-card.js
+++ practice-card.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from 'react';
 import './Card.css'
 
 export default function Card(props) {
     return (
         <div className='Card'>
+            <button type='button'
+            onClick={() => props.onClickDelete(props.id)}
+            >
+                Delete
+            </button>
             <h3>{props.title}</h3>
             <p>{props.content}</p>
         </div>
     )
+}
+
+Card.propTypes = {
+    onClickDelete: () => {}
 }
